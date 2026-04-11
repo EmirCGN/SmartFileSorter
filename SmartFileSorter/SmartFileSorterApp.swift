71b04227a00624fa02adaed9b5df6e2b4c9d6407
EmirCGN <koroglumemir@gmail.com>
2026-04-11 22:33:21 +0200

    Initial Commit

---
+++ SmartFileSorter/SmartFileSorterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartFileSorterApp.swift
+//  SmartFileSorter
+//
+//  Created by Emir Köroglu on 11.04.26.
+//
+
+import SwiftUI
+
+@main
+struct SmartFileSorterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
